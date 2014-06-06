b66cc456e2bdeb349b1faaaeced1a004af829df3
Sean Kelly <sean@invisibleduck.org>
2014-06-06 12:29:59 -0700

    Added a standalone yield() routine, since the user should never call scheduler.yield().  Also added a Generator class, which yields values.

--- std/concurrency.d
+++ std/concurrency.d
@@ -1385,6 +1385,241 @@ __gshared Scheduler scheduler;
 
 
 //////////////////////////////////////////////////////////////////////////////
+// Generator
+//////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * If the caller is a Fiber and is not a Generator, this function will call
+ * scheduler.yield() or Fiber.yield(), as appropriate.
+ */
+void yield()
+{
+    auto fiber = Fiber.getThis();
+    if (!(cast(IsGenerator) fiber))
+    {
+        if (scheduler is null)
+        {
+            if (fiber)
+                return Fiber.yield();
+        }
+        else scheduler.yield();
+    }
+}
+
+
+/// Used to determine whether a Generator is running.
+private interface IsGenerator {}
+
+
+/**
+ * A Generator is a Fiber that periodically returns values of type T to the
+ * caller via yield.  This is represented as an InputRange.
+ *
+ * Example:
+ * ---
+ * import std.concurrency;
+ * import std.stdio;
+ *
+ *
+ * void main()
+ * {
+ *     auto tid = spawn(
+ *     {
+ *         while (true)
+ *         {
+ *             writeln(receiveOnly!int());
+ *         }
+ *     });
+ *
+ *     auto r = new Generator!int(
+ *     {
+ *         foreach (i; 1 .. 10)
+ *             yield(i);
+ *     });
+ *
+ *     foreach (e; r)
+ *     {
+ *         tid.send(e);
+ *     }
+ * }
+ * ---
+ */
+class Generator(T) :
+    Fiber, IsGenerator
+{
+    /**
+     * Initializes a generator object which is associated with a static
+     * D function.  The function will be called once to prepare the range
+     * for iteration.
+     *
+     * Params:
+     *  fn = The fiber function.
+     *
+     * In:
+     *  fn must not be null.
+     */
+    this(void function() fn)
+    {
+        super(fn);
+        call();
+    }
+
+
+    /**
+     * Initializes a generator object which is associated with a static
+     * D function.  The function will be called once to prepare the range
+     * for iteration.
+     *
+     * Params:
+     *  fn = The fiber function.
+     *  sz = The stack size for this fiber.
+     *
+     * In:
+     *  fn must not be null.
+     */
+    this(void function() fn, size_t sz)
+    {
+        super(fn, sz);
+        call();
+    }
+
+
+    /**
+     * Initializes a generator object which is associated with a dynamic
+     * D function.  The function will be called once to prepare the range
+     * for iteration.
+     *
+     * Params:
+     *  dg = The fiber function.
+     *
+     * In:
+     *  dg must not be null.
+     */
+    this(void delegate() dg)
+    {
+        super(dg);
+        call();
+    }
+
+
+    /**
+     * Initializes a generator object which is associated with a dynamic
+     * D function.  The function will be called once to prepare the range
+     * for iteration.
+     *
+     * Params:
+     *  dg = The fiber function.
+     *  sz = The stack size for this fiber.
+     *
+     * In:
+     *  dg must not be null.
+     */
+    this(void delegate() dg, size_t sz)
+    {
+        super(dg, sz);
+        call();
+    }
+
+
+    /**
+     * Returns true if the generator is empty.
+     */
+    final bool empty() @property
+    {
+        return m_value is null || state() == State.TERM;
+    }
+
+
+    /**
+     * Obtains the next value from the underlying function.
+     */
+    final void popFront()
+    {
+        call();
+    }
+
+
+    /**
+     * Returns the most recently generated value.
+     */
+    final T front() @property
+    {
+        return *m_value;
+    }
+
+
+private:
+    T*  m_value;
+}
+
+
+/**
+ * Yields a value of type T to the caller of the currently executing
+ * generator.
+ *
+ * Params:
+ *  value = The value to yield.
+ */
+void yield(T)(ref T value)
+{
+    Generator!T cur = cast(Generator!T) Fiber.getThis();
+    if (cur !is null && cur.state() == Fiber.State.EXEC)
+    {
+        cur.m_value = &value;
+        return Fiber.yield();
+    }
+    throw new Exception("yield(T) called with no active generator for the supplied type",
+                        __FILE__, __LINE__);
+}
+
+
+/// ditto
+void yield(T)(T value)
+{
+    yield(value);
+}
+
+
+unittest
+{
+    import core.exception;
+    import std.exception;
+
+    scheduler = new FiberScheduler;
+    scheduler.start(
+    {
+        auto tid = spawn(
+        {
+            int i = 1;
+            for (; true; i++)
+            {
+                assertNotThrown!AssertError(
+                    assert(receiveOnly!int() == i));
+            }
+            assert(i == 3);
+        });
+
+        auto r = new Generator!int(
+        {
+            assertThrown!Exception(yield(2.0));
+            yield(); // ensure this is a no-op
+            yield(1);
+            yield(); // also once something has been yielded
+            yield(2);
+            yield(3);
+        });
+
+        foreach (e; r)
+        {
+            tid.send(e);
+        }
+    });
+    scheduler = null;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
 // MessageBox Implementation
 //////////////////////////////////////////////////////////////////////////////
 
@@ -1677,8 +1912,7 @@ private
                 {
                     return true;
                 }
-                if( scheduler !is null )
-                    scheduler.yield();
+                yield();
                 synchronized( m_lock )
                 {
                     updateMsgCount();
